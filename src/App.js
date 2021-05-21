09dbafaeddd02445ce78a9e23b84f80d5b5e7264
tinyuliu <tinyu1016@gmail.com>
2021-05-21 16:46:05 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useState } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  }
   return (
     <div className="container">
       <div className="card-header">Network Speed Converter</div>
@@ -17,14 +24,24 @@ function App() {
         <div className="converter">
           <div className="flex-1">
             <div className="converter-title">Set</div>
-            <input type="number" className="input-number" min="0" />
+            <input 
+              type="number" 
+              onChange={handleInputChange}
+              value={inputValue}
+              className="input-number" 
+              min="0" />
           </div>
           <span className="angle-icon fa-2x" style={{marginTop: 30}}>
             <i className="fas fa-angle-right" ></i>
           </span>
+          {/* 右側input */}
           <div className="text-right flex-1">
             <div className="converter-title">Show</div>
-            <input type="text" className="input-number text-right" disabled value="125" />
+            <input 
+              type="text" 
+              className="input-number text-right" 
+              disabled 
+              value={inputValue / 8} />
           </div>
         </div>
       </div>
